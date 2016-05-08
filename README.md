b0b06dbdb6f6b1891981c0e70ec6c87aa2642e1f
rot1024 <rot1024@gmail.com>
2016-05-08 20:53:07 +0900

    Add config file

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ REQUIRED: Node.js v6.x
 ```
 git clone https://github.com/rot1024/gochiusa-onedraw-bot
 cd gochiusa-onedraw-bot
+cp config-sample.js config.js
+# please edit config.js
 npm i
 npm start
 ```
---
+++ config-sample.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+
+  twitter: {
+    accessToken: "",
+    secretToken: ""
+  },
+
+  themes: {
+    chatacters: [
+      "ココア",
+      "チノ",
+      "リゼ",
+      "千夜",
+      "シャロ",
+      "マヤ",
+      "メグ",
+      "青山ブルーマウンテン",
+      "モカ"
+    ],
+    animals: [
+      "ティッピー",
+      "ワイルドギース",
+      "あんこ"
+    ]
+  }
+
+};
