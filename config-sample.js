d82923f72a2df0e8ce9fe95b81b40b18822326b6
rot1024 <rot1024@gmail.com>
2016-05-08 23:50:44 +0900

    Change config structure

--- config-sample.js
+++ config-sample.js
@@ -3,27 +3,29 @@
 module.exports = {
 
   twitter: {
-    accessToken: "",
-    secretToken: ""
+    consumer_key: "",
+    consumer_secret: "",
+    access_token: "",
+    access_token_secret: ""
   },
 
-  themes: {
-    chatacters: [
-      "ココア",
-      "チノ",
-      "リゼ",
-      "千夜",
-      "シャロ",
-      "マヤ",
-      "メグ",
-      "青山ブルーマウンテン",
-      "モカ"
-    ],
-    animals: [
-      "ティッピー",
-      "ワイルドギース",
-      "あんこ"
-    ]
-  }
+  timezone: "Asia/Tokyo",
+
+  themeCount: 4,
+
+  themes: [
+    "ココア",
+    "チノ",
+    "リゼ",
+    "千夜",
+    "シャロ",
+    "マヤ",
+    "メグ",
+    "青山ブルーマウンテン",
+    "モカ",
+    "ティッピー",
+    "ワイルドギース",
+    "あんこ"
+  ]
 
 };
