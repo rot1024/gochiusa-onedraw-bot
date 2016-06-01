d4044e46ec293bde3232b7cac29ec67433afa41e
rot1024 <rot1024@gmail.com>
2016-06-01 15:10:56 +0900

    Add getNextContext

--- jobs.js
+++ jobs.js
@@ -8,44 +8,20 @@ const t = new gochiusa.Twitter();
 module.exports = (log, debug) => ({
   generateThemes: () => co(function *() {
 
-    const context = (yield gochiusa.storage.getContext()) || {
-      themes: [],
-      mainThemes: [],
-      subThemes: []
-    };
+    const context = yield gochiusa.storage.getContext();
+    const nextContext = gochiusa.theme.getNextContext(context);
 
-    context.mainThemes = gochiusa.theme.getNextSequence(
-      context.mainThemes,
-      gochiusa.config.mainThemes
-    );
+    yield gochiusa.storage.saveContext(nextContext);
+    yield gochiusa.storage.logThemes(nextContext);
 
-    context.subThemes = gochiusa.theme.getNextSequence(
-      context.subThemes,
-      gochiusa.config.subThemes
-    );
-
-    // Decide and save themes
-
-    context.themes = gochiusa.theme.generateThemes(
-      gochiusa.config.themes,
-      context.mainThemes[0],
-      context.subThemes[0],
-      gochiusa.config.excludePrevThemes ? context.themes : []
-    );
-
-    yield gochiusa.storage.saveContext(context);
-
-    yield gochiusa.storage.logThemes(context);
-
-    log("gentheme", context.themes.join(", "));
+    log("gentheme", nextContext.themes.join(", "));
 
     return context;
+
   }),
   announce: () => co(function *() {
 
     const context = yield gochiusa.storage.getContext();
-
-    // Tweet notice announcement
 
     const text = gochiusa.announcement.getAnnouncement(context, new Date());
     if (!debug) yield t.tweet(text);
@@ -53,6 +29,7 @@ module.exports = (log, debug) => ({
     log("notice");
 
     return context;
+
   }),
   start: () => co(function *() {
 
@@ -64,6 +41,7 @@ module.exports = (log, debug) => ({
     log("start");
 
     return context;
+
   }),
   finish: () => co(function *() {
 
@@ -75,5 +53,6 @@ module.exports = (log, debug) => ({
     log("finish");
 
     return context;
+
   })
 });
